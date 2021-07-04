40816edc02a635686c2b40a56893a6c673691caf
camariana <camariana40@outlook.com>
2021-07-04 11:31:28 +0000

    bloglist frontend, step3

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
     <div>
       <h2>blogs</h2>
 
-      <Login />
+      <Login blogs={blogs} setBlogs={setBlogs} />
 
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react'
 import loginService from '../services/login'
 import blogService from '../services/blogs'
 
-const Login = () => {
+const Login = ({ blogs, setBlogs }) => {
 
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [newBlog, setNewBlog] = useState({ title: '', author: '', url: '' })
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
 
@@ -46,6 +47,41 @@ const Login = () => {
     window.localStorage.removeItem('loggedInBlogAppUser')
   }
 
+  const handleBlogChange = (event) => {
+    const { name, value } = event.target;
+    setNewBlog({
+      ...newBlog, [name]: value
+    })
+  }
+
+  const createBlog = (event) => {
+    event.preventDefault()
+    
+    const { title, author, url } = newBlog;
+    const blogObject = { 
+      title, 
+      author, 
+      url
+    }
+
+    blogService
+      .create(blogObject)
+      .then(returnBlog => {
+        setBlogs(blogs.concat(returnBlog))
+        setNewBlog({ title: '', author: '', url: '' })
+      })
+      .catch(error => {
+        console.log(error.response)
+        setErrorMessage(
+          //`${error.response.data.error}`
+        )
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000)
+      })
+  }
+
+
   const loginForm = () => (
     <form onSubmit={handleLogin}>
       <div>
@@ -72,27 +108,66 @@ const Login = () => {
     </form>
   )
 
-  const loggedIn = () => (
-    <div>
-      <p>{user.name} logged-in</p>
+  const newBlogForm = () => (
+    <form onSubmit={createBlog}>
+      <div>
+        <label htmlFor="title">Title</label>
+        <input
+          id="title"
+          type="text"
+          value={newBlog.title}
+          name="title"
+          onChange={handleBlogChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="author">Author</label>
+        <input
+          id="author"
+          type="text"
+          value={newBlog.author}
+          name="author"
+          onChange={handleBlogChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="url">Url</label>
+        <input
+          id="url"
+          type="text"
+          value={newBlog.url}
+          name="url"
+          onChange={handleBlogChange}
+        />
+      </div>
+      <button type="submit">Create</button>
+    </form>
+  )
 
-      <button onClick={handleLogout}>
-        Logout
-      </button>
-    </div>
+  const loggedIn = () => (
+    <>
+      <div>
+        <p>{user.name} logged-in</p>
+
+        <button onClick={handleLogout}>
+          Logout
+        </button>
+      </div>
+      {newBlogForm()}
+    </>
   )
 
   return (
     <>
-    <p>
-      {errorMessage}
-    </p> 
-    {
-      user === null 
-      ? loginForm()
-      : loggedIn()
-    }
-  </>
+      <p>
+        {errorMessage}
+      </p> 
+      {
+        user === null 
+        ? loginForm()
+        : loggedIn()
+      }
+    </>
   )
 }
 
