7cd96a6278c396f0b702ca7c7820ab5d9a601b1e
camariana <camariana40@outlook.com>
2021-07-09 22:53:57 +0000

    5.6 Blog list frontend, step6 complete

--- src/App.js
+++ src/App.js
@@ -1,22 +1,144 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+
+import Togglable from './components/Togglable'
+
 import Blog from './components/Blog'
-import Login from './components/Login'
+import BlogForm from './components/BlogForm'
+import LoginForm from './components/LoginForm'
+
 import blogService from './services/blogs'
+import loginService from './services/login'
+
+import Success from './components/Notifications/Success';
+import Error from './components/Notifications/Error';
+
+
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [successMessage, setSuccessMessage] = useState(null);
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [user, setUser] = useState(null)
+
+  const noteFormRef = useRef()
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
+    blogService
+      .getAll()
+      .then(initialBlogs =>
+      setBlogs(initialBlogs)
     )  
   }, [])
+
+  useEffect(() => {
+    const loggedInBlogAppUserJSON = window.localStorage.getItem('loggedInBlogAppUser')
+    if (loggedInBlogAppUserJSON) {
+      const user = JSON.parse(loggedInBlogAppUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+
+  
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user  = await loginService.login({ username, password })
+
+      window.localStorage.setItem(
+        'loggedInBlogAppUser', JSON.stringify(user)
+      )
+
+      blogService.setToken(user.token)
+      setUser(user)
+
+      //reset the username and password
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong username or password')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 7000)
+    }
+  }
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedInBlogAppUser')
+    window.location.reload()
+    return false;
+  }
+
+  const addBlog = (blogObject) => {
+    noteFormRef.current.toggleVisibility()
+    blogService
+      .create(blogObject)
+      .then(returnBlog => {
+        setBlogs(blogs.concat(returnBlog))
+        setSuccessMessage(
+          `a new blog ${blogObject.title} by ${blogObject.author}`
+        )
+        setTimeout(() => {
+          setSuccessMessage(null);
+        }, 7000)
+      })
+      .catch(error => {
+        console.log(error.response)
+        setErrorMessage(
+          `${error.response.data.error}`
+        )
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 7000)
+      })
+  }
+
+  
+
+  const loginForm = () => (
+    <Togglable buttonLabel="Login">
+      <LoginForm 
+        username={username}
+        password={password}
+        handleUsernameChange={({ target }) => setUsername(target.value)}
+        handlePasswordChange={({ target }) => setPassword(target.value)}
+        handleSubmit={handleLogin}
+      />
+    </Togglable>
+  )
+
+  const blogForm = () => (
+    <Togglable buttonLabel="New blog" ref={noteFormRef}>
+      <BlogForm createBlog={addBlog} />
+    </Togglable>
+  )
+
 
 
   return (
     <div>   
       <h1>Blogs</h1>
-      <Login blogs={blogs} setBlogs={setBlogs} />
+
+      <Success message={successMessage} />
+      <Error message={errorMessage} />
+
+      {user === null ?
+        loginForm() :
+        <div>
+          <p>{user.name} logged in</p>
+          <button onClick={handleLogout}>
+            Logout
+         </button>
+          {blogForm()}
+        </div>
+      }
 
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
