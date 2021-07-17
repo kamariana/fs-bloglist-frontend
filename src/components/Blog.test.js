ed06e6c8aa723341967620d5f0e9ccdbf6bbd612
camariana <camariana40@outlook.com>
2021-07-17 23:14:42 +0000

    5.14*: Blog list tests, step2

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import { prettyDOM } from '@testing-library/dom'
 import Blog from './Blog'
 
@@ -24,4 +24,23 @@ test('renders content', () => {
   expect(component.container).toHaveTextContent(
     'What we are living for'
   )
+})
+
+test('checks that the details are shown when the button is click', () => {
+  const blog = {
+    title: 'What we are living for',
+    author: 'A. Camariana',
+    url: 'camariana.gm',
+    likes:  10,
+  }
+
+  const component = render (
+    <Blog blog={blog} />
+  )
+
+  const button = component.getByText('View')
+  //console.log(prettyDOM(button))
+  fireEvent.click(button)
+
+  expect(button).toHaveTextContent('Hide')
 })
