fa3ca43f2f3a298b47d9dc6f9abe9f5981db462d
camariana <camariana40@outlook.com>
2021-07-24 04:00:57 +0000

    5.21: bloglist end to end testing, step5

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,13 +1,27 @@
 describe('Blog app', function() {
 
+  /*
+  * E2E tests by controlling the state of the database
+  * make sure to:
+  *  1. create API endpoints to the backend for the test
+  *  2. Make sure your backend is running in test mode by starting it with this command
+  * (previously configured in the package.json file): npm run start:test
+  */
+
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    const user = {
+    const user1 = {
       name: 'A Camariana',
       username: 'camariana',
       password: 'jannata'
     }
-    cy.request('POST', 'http://localhost:3003/api/users', user)
+    cy.request('POST', 'http://localhost:3003/api/users', user1)
+    const user2 = {
+      name: 'E Faalisco',
+      username: 'faalisco',
+      password: 'efaal'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user2)
     cy.visit('http://localhost:3000')
   })
 
@@ -23,7 +37,7 @@ describe('Blog app', function() {
       })
     })
 
-    /* it.only('fails with wrong credentials', function() {
+    it('fails with wrong credentials', function() {
       cy.contains('Login').click()
       cy.get('#username').type('camariana')
       cy.get('#password').type('jannatas')
@@ -33,7 +47,7 @@ describe('Blog app', function() {
         .should('contain', 'Wrong username or password')
         .and('have.css', 'color', 'rgb(255, 0, 0)')
         .and('have.css', 'border-style', 'solid')
-    }) */
+    })
   })
 
   describe('When logged in', function() {
@@ -46,8 +60,8 @@ describe('Blog app', function() {
 
     it('A blog can be created', function() {
       cy.createBlog({
-        title: 'I am not your Yoko Ono', 
-        author: 'Jane Yang', 
+        title: 'I am not your Yoko Ono',
+        author: 'Jane Yang',
         url: 'https://janeyang.org/2021/04/13/in-search-of-dignity-at-work/'
       })
     })
@@ -55,8 +69,8 @@ describe('Blog app', function() {
     describe('a blog exists', function() {
       beforeEach(function() {
         cy.createBlog({
-          title: 'I am not your Yoko Ono', 
-          author: 'Jane Yang', 
+          title: 'I am not your Yoko Ono',
+          author: 'Jane Yang',
           url: 'https://janeyang.org/2021/04/13/in-search-of-dignity-at-work/'
         })
       })
@@ -65,17 +79,82 @@ describe('Blog app', function() {
         cy.contains('I am not your Yoko Ono')
           .get('.btn--show')
           .click()
-        
+
         cy.contains('I am not your Yoko Ono')
           .get('.btn--like')
           .click()
-        
+
         cy.get('.open')
           .contains('Likes 1')
       })
     })
   })
-  
+
+  describe('and several blogs exist', function() {
+    beforeEach(function() {
+      cy.login({
+        username: 'camariana',
+        password: 'jannata'
+      })
+      cy.createBlog({
+        title: 'I am not your Yoko Ono',
+        author: 'Jane Yang',
+        url: 'https://janeyang.org/2021/04/13/in-search-of-dignity-at-work/'
+      })
+      cy.login({
+        username: 'faalisco',
+        password: 'efaal'
+      })
+      cy.createBlog({
+        title: 'Start Your Journey Through The Quran\'s Language',
+        author: 'Norman Ali Khan',
+        url: 'https://dream.bayyinahtv.com/'
+      })
+      cy.createBlog({
+        title: 'University of the People Reviews: What it\'s Like to Study at UoPeople',
+        author: 'U of People',
+        url: 'https://www.uopeople.edu/student-reviews/'
+      })
+      cy.createBlog({
+        title: 'Quotes for Software Engineers',
+        author: 'Damith C. Rajapakse',
+        url: 'https://www.comp.nus.edu.sg/~damithch/pages/SE-quotes.htm'
+      })
+
+      cy.contains('Login').click()
+      cy.get('#username').type('faalisco')
+      cy.get('#password').type('efaal')
+      cy.get('#login-button').click()
+
+      cy.contains('E Faalisco logged in')
+    })
+
+    it('can be deleted by the user who created it', function() {
+      cy.contains('Start Your Journey Through The Quran\'s Language').parent().find('.btn--show').as('showButton')
+      cy.get('@showButton').click()
+
+      cy.get('.btn--delete')
+        .then(buttons => {
+          //console.log('number of buttons', buttons.length)
+          cy.wrap(buttons[0]).click()
+        })
+
+      cy.get('.blog').should('not.contain', 'Start Your Journey Through The Quran\'s Language')
+    })
+
+    it('cannot be deleted by the user who did not create it', function() {
+      cy.contains('I am not your Yoko Ono').parent().find('.btn--show').as('showButton')
+      cy.get('@showButton').click()
+
+      cy.get('.open')
+        .should('not.contain', 'Delete')
+
+      cy.get('@showButton').click()
+    })
+  })
+
 })
+
+
 
 
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -2,23 +2,20 @@ import React, { useState } from 'react'
 
 const Blog = ({ blog, user, updateLikes, deleteBlog }) => {
   const [blogVisible, setBlogVisible] = useState(false)
-  const userName = blog.user.name
-  const loggedInUserName = user.name
 
-  const showDelete = loggedInUserName === userName
-
+  const showDeleteButton = user === null
+    ? ''
+    : user.name === blog.user.name
 
   return (
     <div className="blog">
       <div className="blog__summary">
-        <div>
-          <h2 className="blog__title">
-            {blog.title}
-          </h2>
-          <span className="blog__sub-title">
-            {blog.author}
-          </span>
-        </div>
+        <h2 className="blog__title">
+          {blog.title}
+        </h2>
+        <span className="blog__sub-title">
+          {blog.author}
+        </span>
         <button className="btn btn--show" onClick={() => setBlogVisible(!blogVisible)}>
           {blogVisible ? 'Hide' : 'View'}
         </button>
@@ -26,13 +23,13 @@ const Blog = ({ blog, user, updateLikes, deleteBlog }) => {
       <div className={ blogVisible ? 'open' : 'close' }>
         {blog.url} <br />
         Likes {blog.likes} <br />
-        {userName}
+        {blog.user.name}
         <div className="blog__footer">
           <button className="btn btn--like" onClick={updateLikes}>
-          Like
+            Like
           </button>
-          {showDelete && (
-            <button className="btn" onClick={deleteBlog}>
+          {showDeleteButton && (
+            <button className="btn btn--delete" onClick={deleteBlog}>
               Delete
             </button>
           )}
